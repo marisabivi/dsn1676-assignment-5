675a902690b95bcf7f15562c23532cd8d36811ea
marisabivi <biv.marisa@yahoo.com>
2015-02-26 16:17:49 -0500

    added js var and inputs

--- js/main.js
+++ js/main.js
@@ -1,2 +1,16 @@
+var $rectangle = $('.rectangle');
 var $form = $('form');
 var $input = $('#todo');
+
+$form.on('submit', function (eventObject) {
+    eventObject.preventDefault();
+    
+    $theCircle.css('list', $input.val());
+    
+    var $theH2 = $('<h2>');
+    $theH2.html($input.val());
+    $theCircle.append($theH2);
+    
+    $input.val('');
+});
+
