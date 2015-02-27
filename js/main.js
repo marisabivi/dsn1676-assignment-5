36a69111849440e2988f71e55d5f656ec3ecb95c
marisabivi <biv.marisa@yahoo.com>
2015-02-26 23:54:35 -0500

    trying to use li for list

--- js/main.js
+++ js/main.js
@@ -1,16 +1,28 @@
-var $rectangle = $('.rectangle');
+//var $rectangle = $('.rectangle');
 var $form = $('form');
 var $input = $('#todo');
 
-$form.on('submit', function (eventObject) {
+//$form.on('submit', function (eventObject) {
+    //eventObject.preventDefault();
+    
+    //$rectangle.css('todo', $input.val());
+    
+    //var $theH2 = $('<h2>');
+    //$theH2.html($input.val());
+    //$rectangle.append($theH2);
+    
+    //$input.val('');
+//});
+
+var $add = $('.add');
+var $list = $('.list');
+
+$add.on('click', function (eventObject) {
     eventObject.preventDefault();
     
-    $rectangle.css('list', $input.val());
-    
     var $theH2 = $('<h2>');
-    $theH2.html($input.val());
+    $list.append($input.val());
     $rectangle.append($theH2);
     
     $input.val('');
 });
-
