2920e2327303fc3ec59754d11ada6032e6fdf5a6
marisabivi <biv.marisa@yahoo.com>
2015-03-04 13:53:15 -0500

    new js (complete removal of old)

--- js/main.js
+++ js/main.js
@@ -1,28 +1,26 @@
-//var $rectangle = $('.rectangle');
+var $toDoList = $('.todo');
+
 var $form = $('form');
 var $input = $('#todo');
+var $ul = $('.ul');
+var $li = $('.li');
 
-//$form.on('submit', function (eventObject) {
-    //eventObject.preventDefault();
-    
-    //$rectangle.css('todo', $input.val());
-    
-    //var $theH2 = $('<h2>');
-    //$theH2.html($input.val());
-    //$rectangle.append($theH2);
-    
-    //$input.val('');
-//});
 
-var $add = $('.add');
-var $list = $('.list');
-
-$add.on('click', function (eventObject) {
+$form.on('submit', function (eventObject) {
     eventObject.preventDefault();
     
-    var $theH2 = $('<h2>');
-    $list.append($input.val());
-    $rectangle.append($theH2);
+    $ul.css('ul', $input.val());
+    
+    var $li = $('<li>');
+    $li.html($input.val());
+    
+    $ul.append($li);
     
     $input.val('');
+});
+
+$input.val('');
+
+$ul.on('click', 'li', function () {
+    $('this').toggleClass('done');
 });
